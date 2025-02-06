22ea564b1813385685b5b07b3c159e5995a34a9b
YashwantXDX <yashwant.choure73546@gmail.com>
2025-02-06 13:25:32 +0530

    Left Side of Binary Tree

---
+++ BinaryTree/LeftViewOfBinaryTree.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+    Problem Link :- https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1?page=1&difficulty%5B%5D=0&category%5B%5D=Tree&sortBy=submissions
+
+*/
+
+class Node{
+    public:
+        int data;
+        Node *left, *right;
+
+        Node(int data){
+            this -> data = data;
+            left = right = NULL;
+        }
+};
+
+Node *binaryTree(){
+
+    int x; cin >> x;
+
+    if(x == -1) return NULL;
+
+    Node *temp = new Node(x);
+
+    // Left Side
+    cout << "Enter Left Side of " << x << " : ";
+    temp -> left = binaryTree();
+
+    // Right Side
+    cout << "Enter Right Side of " << x << " : ";
+    temp -> right = binaryTree();
+
+    return temp;
+}
+
+void leftOfTree(Node *root, vector<int> &ans){
+
+    queue<Node *> q;
+    Node *temp;
+
+    q.push(root);
+
+    while(!q.empty()){
+        int size = q.size();
+        ans.push_back(q.front() -> data);
+
+        while(size--){
+            temp = q.front();
+            q.pop();
+
+            if(temp -> left)
+                q.push(temp -> left);
+
+            if(temp -> right)
+                q.push(temp -> right);
+
+        }
+    }
+
+}
+
+vector<int> leftView(Node *root) {
+    
+    vector<int> ans;
+    
+    leftOfTree(root, ans);
+
+    return ans;
+
+}
+
+
+int main(){
+
+    cout << "Enter Root Node : ";
+    Node *root = binaryTree();
+
+    vector<int> ans = leftView(root);
+
+    for(int i : ans)
+        cout << i << " ";
+}
