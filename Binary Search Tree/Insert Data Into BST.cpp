eafed075e849680152bb1b08cd3d59acf9e24663
YashwantXDX <yashwant.choure73546@gmail.com>
2025-02-13 12:23:04 +0530

    Insert Data in BST

---
+++ Binary Search Tree/Insert Data Into BST.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+    Problem Link :- 
+
+    Refer Video :- https://www.youtube.com/watch?v=pMHXL46exp4&list=PLQEaRBV9gAFu4ovJ41PywklqI7IyXwr01&index=112
+
+*/
+
+class Node{
+    public:
+        int data;
+        Node *left, *right;
+
+        Node(int data){
+            this -> data = data;
+            left = right = NULL;
+        }
+};
+
+Node *insertData(Node *root, int target){
+
+    if(!root){
+        return new Node(target);
+    }
+
+    if(root -> data > target){
+        root -> left = insertData(root -> left, target);
+    }
+
+    else{
+        root -> right = insertData(root -> right, target);
+    }
+
+    return root;
+}
+
+void printData(Node *root){
+
+    while(root){
+
+        if(!root -> left){
+            cout << root -> data << " ";
+            root = root -> right;
+        }
+
+        else{
+
+            Node *curr = root -> left;
+
+            while(curr -> right && curr -> right != root)
+                curr = curr -> right;
+            
+            if(curr -> right == NULL){
+                curr -> right = root;
+                root = root -> left;
+            }
+
+            else{
+                cout << root -> data << " ";
+                root = root -> right;
+                curr -> right = NULL;
+            }
+
+        }
+
+    }
+
+}
+
+int main(){
+
+    vector<int> arr = {82,1,106,58,89,56,68,92,24,35,26};
+    Node *root = NULL;
+
+    for(int i = 0; i < arr.size(); i++){
+        root = insertData(root, arr[i]);
+    }
+
+    root = insertData(root, 26);
+
+    printData(root);
+    
+}
+
